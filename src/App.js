bca31619570ec89cd122633f042099684aec4058
Teja Chundru <tejachundru@Opstechs-Mac-mini.local>
2022-01-06 09:55:32 +0530

    added cname

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Teja Chundru</p>
+        <Typography variant="h4">Teja Chundru</Typography>
       </header>
     </div>
   );
