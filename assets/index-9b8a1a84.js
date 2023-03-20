4967eb4e975568849962e3019f10b8443fc65c8d
teja <teja@opstech.in>
2023-03-20 22:40:21 +0530

    Updates

---
+++ assets/index-9b8a1a84.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-29ff063a.js";import{T as r}from"./Typography-41b9b730.js";const a=()=>o.jsx("div",{children:o.jsx(r,{children:"asd"})});export{a as default};
--- assets/index-c6dfdaa8.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-14e38e8b.js";import{T as r}from"./Typography-64f7778c.js";const a=()=>o.jsx("div",{children:o.jsx(r,{children:"asd"})});export{a as default};
